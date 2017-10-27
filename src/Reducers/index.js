fb4fafb1393a48b95c6eefd1101eb46bc284c5f7
Cheryln <cherylnbarber@gmail.com>
2017-10-27 14:23:01 -0700

    working on redo

--- src/Reducers/index.js
+++ src/Reducers/index.js
@@ -1,11 +1,2 @@
-import { combineReducers } from 'redux';
-import BooksReducer from './reducer-books'
-
-const rootReducer = combineReducers({
-    books: BooksReducer
-});
-
-export default rootReducer;
-
-
+import cobmineReducers from 'redux';
 
