c7718f426610a0e1524439423ef2c64fb1f5e8c1
Cheryln <cherylnbarber@gmail.com>
2017-10-27 15:22:31 -0700

    Working on mapSateToProps

--- src/containers/book_list.js
+++ src/containers/book_list.js
@@ -3,19 +3,29 @@ import { connect } from 'react-redux'
 
 class BookList extends Component {
 
-   listOfBooks =  this.props.books.map((el, i)=>{
-        return <li key={i}>{el.title}</li>
-    })
+    listOfBooks() {
+        return this.props.books.map((el, i) => {
+            return <li key={i}>{el.title}</li>
+        })
+
+    }
+
+
 
     render() {
         return (
             <div>
-                {listOfBooks}
+                {listOfBooks()}
             </div>
         )
     }
 }
 
 mapStateToProps(state){
-    return state.books
+    return {
+        books: state.books
+    }
 }
+
+
+export default connect(mapStateToProps)(BookList)
